3c2c7e30856c9978c6c5ea64eb029abc8c93bbc6
mattisaa <mattis_araya@hotmail.com>
2021-04-20 21:44:44 +0200

    Fix visualization

--- src/pages/VisualizeSongs.tsx
+++ src/pages/VisualizeSongs.tsx
@@ -1,6 +1,12 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import styled from "styled-components";
 import { ItemsEntity } from "../types/types";
+import Text, { TextTypes } from "../components/Text";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Image = styled.img`
   object-fit: contain;
@@ -12,18 +18,38 @@ export function VisualizeSongs({
   tracks: ItemsEntity[];
 }): ReactElement {
   const [currentSongUri, setCurrentSongUri] = useState<string>();
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [isVisualizingFinished, setIsVisualizingFinished] = useState(false);
+
   useEffect(() => {
     tracks.forEach((item, index) =>
-      setTimeout(
-        () => setCurrentSongUri(item.track.album.images[1].url),
-        index * 150
-      )
+      setTimeout(() => {
+        setCurrentSongUri(item.track.album.images[1].url);
+        setCurrentIndex(index + 1);
+      }, index * 150)
     );
   }, []);
 
+  if (currentIndex === tracks.length && !isVisualizingFinished) {
+    setTimeout(() => {
+      setIsVisualizingFinished(true);
+    }, 250);
+  }
+
   return (
-    <div>
-      <Image src={currentSongUri} />
-    </div>
+    <Container>
+      {isVisualizingFinished ? null : (
+        <div>
+          <Image src={currentSongUri} />
+          <Text
+            type={TextTypes.xlarge}
+            bold
+            style={{ textAlign: "center", marginTop: 20 }}
+          >
+            {currentIndex}/{tracks.length}
+          </Text>
+        </div>
+      )}
+    </Container>
   );
 }
